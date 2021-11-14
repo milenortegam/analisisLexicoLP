3e5c703714b87f4c205f63b904cc785d6a8a37e2
Gabriela Pazmiño Guerrero <gnpazmin@espol.edu.ec>
2021-11-14 11:06:26 -0500

    arreglo de lapabras reservadas

--- analizadorLexico.py
+++ analizadorLexico.py
@@ -2,6 +2,45 @@ import ply.lex as lex
 
 #Milen Ortega Mautong
 
+reservadas = {
+    'alias': 'ALIAS',
+    'begin': 'BEGIN',
+    'class': 'CLASS',
+    'do': 'DO',
+    'end': 'END',
+    'false': 'FALSE',
+    'module': 'MODULE',
+    'not': 'NOT',
+    'rescue': 'RESCUE',
+    'self': 'SELF',
+    'true': 'TRUE',
+    'until': 'UNTIL',
+    'yield': 'YIELD',
+    'and': 'AND',
+    'break': 'BREAK',
+    'def': 'DEF',
+    'else': 'ELSE',
+    'for': 'FOR',
+    'next': 'NEXT',
+    'or': 'OR',
+    'retry': 'RETRY',
+    'super': 'SUPER',
+    'undef': 'UNDEF',
+    'when': 'WHEN',
+    'file': 'FILE',
+    'case': 'CASE',
+    'defined': 'DEFINED',
+    'elsif': 'ELSIF',
+    'ensure': 'ENSURE',
+    'if': 'IF',
+    'nil': 'NIL',
+    'redo': 'REDO',
+    'return': 'RETURN',
+    'unles': 'UNLES',
+    'while': 'WHILE',
+    'line': 'LINE',
+}
+
 
 tokens = (
     'NUMERO',
@@ -14,6 +53,7 @@ tokens = (
     'FLOTANTE',
 ) + tuple(reservadas.values())
 
+
 t_MAS = r'\+'
 t_MENOS = r'-'
 t_MULTIPLICACION = r'\*'
@@ -22,50 +62,6 @@ t_IZQPAREN = r'\('
 t_DERPAREN = r'\)'
 t_FLOTANTE = r'\d+\.\d+'
 t_NUMERO = r'\d+'
-
-
-reservadas = {
-    'alias': 'alias',
-    'begin': 'begin',
-    'class': 'class',
-    'do': 'do',
-    'end': 'END',
-    'false': 'false',
-    'module': 'module',
-    'not': 'not',
-    'rescue': 'rescue',
-    'self': 'self',
-    'true': 'true',
-    'until': 'until',
-    'yield': 'yield',
-    'and': 'and',
-    'break': 'break',
-    'def': 'def',
-    'else': 'else',
-    'end': 'end',
-    'for': 'for',
-    'next': 'next',
-    'or': 'or',
-    'retry': 'retry',
-    'super': 'super',
-    'undef': 'undef',
-    'when': 'when',
-    'file': '_ _FILE_ _',
-    'begin': 'BEGIN',
-    'case': 'case',
-    'defined': 'defined?',
-    'elsif': 'elsif',
-    'ensure': 'ensure',
-    'if': 'if',
-    'nil': 'nil',
-    'redo': 'redo',
-    'return': 'return',
-    'unles': 'unles',
-    'while': 'while',
-    'line': '_ _LINE_ _',
-}
-
-
 
 
 
@@ -78,6 +74,10 @@ def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reservadas.get(t.value, 'ID')
     return t
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
 
 
 lexer = lex.lex()
