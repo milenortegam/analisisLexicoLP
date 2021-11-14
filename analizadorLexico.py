927d724ccd01d85251695830726870f9cceb3be0
Gabriela Pazmiño Guerrero <gnpazmin@espol.edu.ec>
2021-11-14 16:49:55 -0500

    arreglo de datos primitivos

--- analizadorLexico.py
+++ analizadorLexico.py
@@ -1,6 +1,6 @@
 import ply.lex as lex
 
-#Milen Ortega Mautong
+# Milen Ortega Mautong
 
 reservadas = {
     'alias': 'alias',
@@ -40,35 +40,33 @@ reservadas = {
     'while': 'while',
 }
 
-
 tokens = (
-    'MAS',
-    'MENOS',
-    'MULTIPLICACION',
-    'DIVISION',
-    'IZQPAREN',
-    'DERPAREN',
-    'FLOTANTE',
-    'VGLOBALES',
-    'VLOCALES',
-    'VCLASE',
-    'VINSTANCIA',
-    'CONSTANTES',
-#Gabriela Pazmiño Guerrero
-    'NUMERO',
-    'IMAGINARIO',
-    'BOOLEANOV',
-    'BOOLEANOF',
-    'CADENAS',
-    'ARREGLOS',
-    'MAPAS',
-    'PUNTO',
-    'COMA',
-    'INTERROGACION',
-    'ADMIRACION',
-    'COMENTARIO'
-) + tuple(reservadas.values())
-
+             'MAS',
+             'MENOS',
+             'MULTIPLICACION',
+             'DIVISION',
+             'IZQPAREN',
+             'DERPAREN',
+             'FLOTANTE',
+             'VGLOBALES',
+             'VLOCALES',
+             'VCLASE',
+             'VINSTANCIA',
+             'CONSTANTES',
+             # Gabriela Pazmiño Guerrero
+             'NUMERO',
+             'FLOTANTES',
+             'BOOLEANOV',
+             'BOOLEANOF',
+             'CADENAS',
+             'ARREGLOS',
+             'MAPAS',
+             'PUNTO',
+             'COMA',
+             'INTERROGACION',
+             'ADMIRACION',
+             'COMENTARIO'
+         ) + tuple(reservadas.values())
 
 t_MAS = r'\+'
 t_MENOS = r'-'
@@ -77,82 +75,74 @@ t_DIVISION = r'/'
 t_IZQPAREN = r'\('
 t_DERPAREN = r'\)'
 
-#Milen Ortega Mautong
-#t_VGLOBALES = r'^\$[a-z]+[_a-zA-Z0-9]*'
-#t_VLOCALES = r'^[a-z]+[_a-zA-Z0-9]*'
-#t_VCLASE = r'^@@[a-z]+[_a-zA-Z0-9]*'
-#t_VINSTANCIA = r'^@[a-z]+[_a-zA-Z0-9]*'
-#t_CONSTANTES = r'^[A-Z]+[_a-zA-Z0-9]*'
+
+# Milen Ortega Mautong
+# t_VGLOBALES = r'^\$[a-z]+[_a-zA-Z0-9]*'
+# t_VLOCALES = r'^[a-z]+[_a-zA-Z0-9]*'
+# t_VCLASE = r'^@@[a-z]+[_a-zA-Z0-9]*'
+# t_VINSTANCIA = r'^@[a-z]+[_a-zA-Z0-9]*'
+# t_CONSTANTES = r'^[A-Z]+[_a-zA-Z0-9]*'
 
 def t_VGLOBALES(t):
     r'[\$][a-z]+[_a-zA-Z0-9]*'
     t.type = reservadas.get(t.value, 'VGLOBALES')
     return t
 
+
 def t_VLOCALES(t):
     r'[a-z]+[_a-zA-Z0-9]*'
     t.type = reservadas.get(t.value, 'VLOCALES')
     return t
+
 
 def t_VCLASE(t):
     r'[@][@][a-z]+[_a-zA-Z0-9]*'
     t.type = reservadas.get(t.value, 'VCLASE')
     return t
 
+
 def t_VINSTANCIA(t):
     r'[@][a-z]+[_a-zA-Z0-9]*'
     t.type = reservadas.get(t.value, 'VINSTANCIA')
     return t
+
 
 def t_CONSTANTES(t):
     r'[A-Z]+[_a-zA-Z0-9]*'
     t.type = reservadas.get(t.value, 'CONSTANTES')
     return t
 
-#Gabriela Pazmiño Guerrero
-#t_NUMERO = r'\d+'
-#t_FLOTANTES = r'\d+\.+\d*'
-#t_IMAGINARIO = r'\d+\+\d'
-#t_BOOLEANOV = r'\true'
-#t_BOOLEANOF = r'\false'
-#t_CADENAS = r'"[a-zA-Z0-9\s,]*"'
-#t_ARREGLOS = r"\[(('([a-zA-z\s])*'|[0-9]+|[0-9]+,?[0-9]*),?)+\]"
-#t_MAPAS = r"\{((\"|')?[a-zA-Z_][a-zA-Z0-9_\s]*(\"|')?(\:|\=>)([0-9]|[1-9][0-9]*|(\"|')[\w\s]+(\"|')),?)+\}"
+
+# Gabriela Pazmiño Guerrero
+# t_FLOTANTES = r'\d+\.\d+'
+# t_NUMERO = r'\d+'
+# t_CADENAS = r'"[a-zA-Z0-9\s,]*"'
+# t_ARREGLOS = r"\[(('([a-zA-z\s])*'|[0-9]+|[0-9]+,?[0-9]*),?)+\]"
+# t_MAPAS = r"\{((\"|')?[a-zA-Z_][a-zA-Z0-9_\s]*(\"|')?(\:|\=>)([0-9]|[1-9][0-9]*|(\"|')[\w\s]+(\"|')),?)+\}"
 t_PUNTO = r'\.'
 t_COMA = r'\,'
 t_INTERROGACION = r'\?'
 t_ADMIRACION = r'\!'
 t_COMENTARIO = r"\#.*"
 
+
+def t_FLOTANTES(t):
+    r'\d+\.\d*'
+    t.value = float(t.value)
+    return t
+
+
 def t_NUMERO(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-def t_FLOTANTES(t):
-    r'\d+\.+\d*'
-    t.value = int(t.value)
-    return t
-
-def t_IMAGINARIO(t):
-    r'\d+\+\d'
-    t.value = int(t.value)
-    return t
-
-def t_t_BOOLEANOV(t):
-    r'\true'
-    t.value = int(t.value)
-    return t
-
-def t_t_BOOLEANOF(t):
-    r'\false'
-    t.value = int(t.value)
-    return t
 
 def t_ARREGLOS(t):
     r"\[(('([a-zA-z\s])*'|[0-9]+|[0-9]+,?[0-9]*),?)+\]"
     t.value = int(t.value)
     return t
+
 
 def t_MAPAS(t):
     r"\{((\"|')?[a-zA-Z_][a-zA-Z0-9_\s]*(\"|')?(\:|\=>)([0-9]|[1-9][0-9]*|(\"|')[\w\s]+(\"|')),?)+\}"
@@ -160,28 +150,30 @@ def t_MAPAS(t):
     return t
 
 
-#Escribe tu nombre
+# Escribe tu nombre
 #
 #
 #
 #
 
-#General
+# General
 def t_newline(t):
-     r'\n+'
-     t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-t_ignore  = ' \t'
+
+t_ignore = ' \t'
 
 lexer = lex.lex()
 
- # Test it out
+# Test it out
 data = '''
-    3 + 4 * 10
+    3 + 4.4 * 10
     + -20 *2
     $var_global 
     @@var_clase
@@ -190,13 +182,13 @@ data = '''
     var_local
     if
 '''
- 
- # Give the lexer some input
+
+# Give the lexer some input
 lexer.input(data)
- 
- # Tokenize
+
+# Tokenize
 while True:
     tok = lexer.token()
-    if not tok: 
-        break      # No more input
+    if not tok:
+        break  # No more input
     print(tok)
