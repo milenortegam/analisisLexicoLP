d7f02806ba2750fae32c7ff30b1136763c1b734d
Gabriela Pazmiño Guerrero <gnpazmin@espol.edu.ec>
2021-11-21 15:09:54 -0500

    definicion de reglas

--- analizadorSintactico.py
+++ analizadorSintactico.py
@@ -1,52 +1,63 @@
- import ply.yacc as yacc
- 
- # Get the token map from the lexer.  This is required.
- from calclex import tokens
- 
- def p_expression_plus(p):
-     'expression : expression PLUS term'
-     p[0] = p[1] + p[3]
- 
- def p_expression_minus(p):
-     'expression : expression MINUS term'
-     p[0] = p[1] - p[3]
- 
- def p_expression_term(p):
-     'expression : term'
-     p[0] = p[1]
- 
- def p_term_times(p):
-     'term : term TIMES factor'
-     p[0] = p[1] * p[3]
- 
- def p_term_div(p):
-     'term : term DIVIDE factor'
-     p[0] = p[1] / p[3]
- 
- def p_term_factor(p):
-     'term : factor'
-     p[0] = p[1]
- 
- def p_factor_num(p):
-     'factor : NUMBER'
-     p[0] = p[1]
- 
- def p_factor_expr(p):
-     'factor : LPAREN expression RPAREN'
-     p[0] = p[2]
- 
- # Error rule for syntax errors
- def p_error(p):
+import ply.yacc as yacc
+
+from analizadorLexico import tokens
+
+'''Reglas definidas por Milen Ortega
+asignacion
+append
+split
+slicing
+puts
+comentarios
+'''
+'''Reglas definidas por Gabriela Pazmiño
+sentenciaIf
+sentenciaFOR
+sentenciaWHILE
+sentenciaCASE
+unless
+'''
+
+'''Reglas definidas por Hayleen Carrillo
+sentenciaBegin
+sentenciaFuncion
+pop
+push
+clear
+'''
+
+
+def p_base(p):
+    '''base : sentencias
+                | sentencias base
+    '''
+
+def p_sentencias(p):
+    ''' sentencias : asignacion
+                    | append
+                    | split
+                    | slicing
+
+                    | sentenciaIf
+                    | sentenciaFOR
+                    | sentenciaWHILE
+                    | sentenciaCASE
+                    | unless
+
+                    | sentenciaBegin
+                    | sentenciaFuncion
+                    | pop
+                    | push
+                    | clear
+
+                    | puts
+                    | comentarios
+
+    '''
+# Error rule for syntax errors
+def p_error(p):
      print("Syntax error in input!")
- 
- # Build the parser
- parser = yacc.yacc()
- 
- while True:
-    try:
-        s = raw_input('calc > ')
-    except EOFError:
-        break
-    if not s: continue
-    result = parser.parse(s)
-    print(result)
+
+
+# Build the parser
+parser = yacc.yacc()
