71ebfcabfc6a41b1a3ecf5d124b2b1cd1535d14c
Gabriela Pazmiño Guerrero <gnpazmin@espol.edu.ec>
2021-11-30 21:43:18 -0500

    semantico operaciones completo

--- analizadorSintactico.py
+++ analizadorSintactico.py
@@ -319,6 +319,46 @@ def p_reglaSemanticaCondiciones(p):
         p[0] = p[1] and p[3]
 
 
+#Fin Milen Ortega
+
+#Inicio Gabriela Pazmiño
+
+def p_regla_Semantica_Operaciones_Matematicas(p):
+    '''p_regla_Semantica_Operaciones_Matematicas : valor_Matematico MAS valor_Matematico
+                                                  | valor_Matematico MENOS valor_Matematico
+                                                  | valor_Matematico MULTIPLICACION valor_Matematico
+                                                  | valor_Matematico DIVISION valor_Matematico
+                                                  | valor_Matematico MODULO valor_Matematico
+                                                  | valor_Matematico EXPONENCIAL valor_Matematico
+
+    '''
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
+    elif p[2] == '*':
+        p[0] = p[1] * p[3]
+    elif p[2] == '/':
+        p[0] = p[1] / p[3]
+    elif p[2] == '%':
+        p[0] = p[1] % p[3]
+    elif p[2] == '**':
+        i = p[3]
+        p[0] = p[1]
+        while i > 1:
+            p[0] *= p[1]
+            i -= 1
+
+
+def p_regla_Semantica_Operaciones(p):
+    '''p_regla_Semantica : valor operadorMat valor
+                              | operacionMat operadorMat valor
+    '''
+    if (type(p[1]) != type(p[3])):
+        print("No se puede realizar la operacion matematica por que los valores no son del mismo tipo")
+
+#Fin Gabriela Pazmiño
+
 
 
 #contruccion del parser
@@ -353,20 +393,3 @@ for s in data:
 #    result = parser.parse(s)
 #    print(result)
 
-#reglas Semanticas
-def p_regla_Semantica_Operaciones_Matematicas(p):
-    '''p_regla_Semantica_Operaciones_Matematicas : valor_Matematico MAS valor_Matematico
-                                                  | valor_Matematico MENOS valor_Matematico
-                                                  | valor_Matematico MULTIPLICACION valor_Matematico
-                                                  | valor_Matematico DIVISION valor_Matematico
-                                                  | valor_Matematico EXPONENCIAL valor_Matematico
-                                                  | valor_Matematico MODULO valor_Matematico
-    '''
-
-
-def p_regla_Semantica_Operaciones(p):
-    '''p_regla_Semantica : valor operadorMat valor
-                              | operacionMat operadorMat valor
-    '''
-    if (type(p[1]) != type(p[3])):
-        print("No se puede realizar la operacion matematica por que los valores no son del mismo tipo")
